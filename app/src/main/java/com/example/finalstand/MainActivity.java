08020acf705e4139274c1cc1c64eed4f09da3db2
quenchglo <qianw01@gmail.com>
2022-04-17 21:11:23 -0400

    did some stuff with the navigation

--- app/src/main/java/com/example/finalstand/MainActivity.java
+++ app/src/main/java/com/example/finalstand/MainActivity.java
@@ -3,8 +3,11 @@ import android.graphics.Typeface;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
